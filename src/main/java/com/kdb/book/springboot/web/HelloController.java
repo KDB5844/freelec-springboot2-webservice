7bd9ea9b70bab1c3f1d33ea46b9f2371fb329843
Subin Cho <sbjo7713@gmail.com>
2022-01-29 02:52:10 +0900

    build.gradle 수정 및 HelloController TDD 작성

--- src/main/java/com/kdb/book/springboot/web/HelloController.java
+++ src/main/java/com/kdb/book/springboot/web/HelloController.java
@@ -1,4 +1,14 @@
 package com.kdb.book.springboot.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController     //JSON을 반환하는 컨트롤러로 만들어 줌(ResponseBody + Controller)
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
 }
