183454f4718d0515b6ea4892d7d5673e8309988f
KDB5844 <sbjo7713@gmail.com>
2022-02-05 14:31:45 +0900

    머스테치 의존성 추가, index 페이지 개발, Test 코드 작성

---
+++ src/main/java/com/kdb/book/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.kdb.book.springboot.web;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
