8ea731520a6de0b7bca7d14e7e3b65a3041ed1c2
KDB5844 <sbjo7713@gmail.com>
2022-02-06 10:54:39 +0900

    머스테치 Layout 구성, 게시글 저장 API, View 개발

--- src/main/java/com/kdb/book/springboot/web/IndexController.java
+++ src/main/java/com/kdb/book/springboot/web/IndexController.java
@@ -1,6 +1,5 @@
 package com.kdb.book.springboot.web;
 
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,6 +9,11 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 
 }
